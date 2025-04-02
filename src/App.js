f4c4513c807b62901897c4228f029fdba95e07f0
Muhammad Rizwan <Crizi2714@gmail.com>
2025-04-03 03:02:24 +0500

    Added all missing files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Table } from '@mui/material';
 import './App.css'
+import MainDash from './components/MainDash/MainDash';
+import Sidebar from './components/Sidebar/Sidebar';
+import RightSide from './components/RightSide/RightSide';
 
 function App() {
   return (
-    <div className="App">
-        Subscribe Zainkeepscode
+    <div className="app">
+        <div className="appGlass">
+          <Sidebar />
+          <MainDash />
+          <RightSide/>
+        </div>
     </div>
   );
 }
